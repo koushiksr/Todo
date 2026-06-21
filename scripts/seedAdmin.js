70f91bfa717982d466c2e8d7736ece935404dc7d
koushik <koushik@Byateshs-MacBook-Air.local>
2026-06-21 23:51:56 +0530

    fix: Remove deprecated mongoose options

--- scripts/seedAdmin.js
+++ scripts/seedAdmin.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const seedAdmin = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     
     console.log('Connected to MongoDB');
 
